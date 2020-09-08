5401efa43ab031709bd063966bf04e86aa4d4e34
Batsaikhan Ulambayar <Batsaikhan@Sugarmaas-MacBook-Pro.local>
2020-09-07 21:05:45 -0500

    user login registration setup

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,17 @@
+class RegistrationsController < ApplicationController
+    def create
+        user = User.create!(
+            email: params[:user][:email],
+            password: params[:user][:password],
+            password_confirmation: params[:user][:password_confirmation]
+        )
+        if user
+            session[:user_id] = user.id
+            render json: { 
+                status: 'REGISTRATION_SUCCESS'
+                user: user
+            }
+        else
+            render json: { 'REGISTRATION_FAILURE' }
+    end
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(email: params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            render json: { 
+                status: 'LOGIN_SUCCESS',
+                logged_in: true,
+                user: user
+            }
+        else
+            render json: { status: 'LOGIN_FAILURE' }
+        end
+    end
+end
